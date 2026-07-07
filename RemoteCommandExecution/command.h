7b3f99cd2e94f267b04962ca89282e740294801c
Dipak Kumar Gupta <dipakkumargupta893@gmail.com>
2026-07-07 07:39:33 +0545

    Remove old RemoteCommandExecution folder

--- RemoteCommandExecution/command.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMMAND_H
-#define COMMAND_H
-
-int isAllowedCommand(const char *command);
-void executeCommand(const char *command, char *output);
-
-#endif
--- RemoteCommandExecution/logger.h
+++
@@ -1,6 +0,0 @@
-#ifndef LOGGER_H
-#define LOGGER_H
-
-void logEvent(const char *event);
-
-#endif
